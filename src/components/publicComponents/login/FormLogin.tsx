ffd3b7a85a61be193feb2d5b53b15fedb50a8c5b
MoisesSegura <moisessegurarojas@gmail.com>
2024-08-26 16:03:21 -0600

    cambios a la pagina del login

--- src/components/publicComponents/login/FormLogin.tsx
+++ src/components/publicComponents/login/FormLogin.tsx
@@ -1,30 +1,43 @@
-import {ButtonLogin} from './ButtonLogin';
+import { ButtonLogin } from './ButtonLogin';
 import Alerta from './Alerta';
 import useLogin from '../../../hooks/useLogin';
 
-
 const FormLogin: React.FC = () => {
 
-    const {email, setEmail, password, setPassword, alerta, handleSubmit} = useLogin();
+    const { email, setEmail, password, setPassword, alerta, handleSubmit } = useLogin();
    
     return (
         <>
-        {alerta.mensaje && <Alerta alerta={alerta} />}
-        <form onSubmit={handleSubmit} className='space-y-4 m-4 justify-center w-5/6 md:w-full'>
-            <div className='flex flex-col'>
-                <label htmlFor="email" className="text-[#DBFF5E] text-left text-lg m-1 font-sans font-bold">Correo electrónico:</label>
-                <input type="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Ingrese su correo electrónico" className=' rounded md:w-4/5 m-1 p-1'  />
-            </div>
-            <div className='flex flex-col '>
-                <label htmlFor="password" className="text-[#DBFF5E] text-left text-lg m-1 font-sans font-bold">Contraseña:</label>
-                <input type="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Ingrese su contraseña" className='rounded md:w-4/5 m-1 p-1' />
-            </div>
-        <ButtonLogin name="Iniciar Sesión"/> 
-        </form>
+            {alerta.mensaje && <Alerta alerta={alerta} />}
+            <form onSubmit={handleSubmit} className="w-full flex flex-col items-center">
+                <div className='flex flex-col w-full md:w-4/5'>
+                    <label htmlFor="email" className="text-[#DBFF5E] text-left text-lg m-1 font-sans font-bold">Correo electrónico:</label>
+                    <input 
+                        type="email" 
+                        id="email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        placeholder="Ingrese su correo electrónico" 
+                        className='rounded w-full p-2'  
+                    />
+                </div>
+                <div className='flex flex-col w-full md:w-4/5'>
+                    <label htmlFor="password" className="text-[#DBFF5E] text-left text-lg m-1 font-sans font-bold">Contraseña:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        placeholder="Ingrese su contraseña" 
+                        className='rounded w-full p-2' 
+                    />
+                </div>
+                <div className='flex justify-center w-full mt-4'>
+                    <ButtonLogin name="Iniciar Sesión" />
+                </div>
+            </form>
         </>
     );
-
-    
 };
 
 export default FormLogin;
--- src/pages/admin/publicRoutes/InicioSesion.tsx
+++ src/pages/admin/publicRoutes/InicioSesion.tsx
@@ -1,41 +1,26 @@
-
-import FormLogin from "../../../components/publicComponents/login/FormLogin"
+import FormLogin from "../../../components/publicComponents/login/FormLogin";
 import { Title } from "../../../components/publicComponents/register/Title";
-import { Subtitle } from "../../../components/publicComponents/register/Subtitle";
-import { Information } from "../../../components/publicComponents/register/Information";
-import { Logos } from "../../../components/publicComponents/register/Logos"
-
 
 export const InicioSesion = () => {
-  
   return (
-   <>
-   <section className="flex flex-col justify-center items-center h-screen w-full md:container md:mx-auto animate-blurred-fade-in">
-      <article className="flex flex-col space-y-2 md:flex-row w-full ">
-        <div className="w-auto md:w-1/2 space-y-2">
-          <Title name="TECHONE" />
-          <Subtitle name="Gestión inteligente, resultados sorprendentes" /> 
-          <Information name="Inicia sesión llenando nuestro formulario o bien iniciando sesión con tus redes." />
-          <Logos />
-        </div>
-          <div className="w-full md:w-1/2 space-y-2 justify-center md:mx-auto">
-            <h1 className="text-white text-xl font-sans m-4 font-bold">Inicia Sesión</h1>
-              <div className="bg-[#82F7FF]/[17%] rounded-md m-4 w-5/6 md:w-2/3 justify-center border border-[#38FF88]" >  
-                <FormLogin/>
-
-                <section className="flex flex-row space-x-8 m-1 justify-center">
-                <p className="text-white">¿No tienes una cuenta aún? <a href="Register" className="text-[#2BFF7F]">Registrate </a></p>
-                <a href="/forgot-password" className="text-white "> Olvide mi contraseña</a>
-
-                </section>
-
-               
-              </div> 
-          
+    <>
+      <section className="flex flex-col justify-center items-center min-h-screen w-full animate-blurred-fade-in">
+        <article className="flex flex-col w-full md:flex-row justify-center items-center space-y-4 md:space-y-0">
+          <div className="w-full md:w-1/2 flex flex-col items-center space-y-4">
+            <Title name="TECHONE" />
+            <div className="bg-[#82F7FF]/[17%] rounded-md w-5/6 md:w-2/3 p-2 border border-[#38FF88]">
+              <FormLogin />
+            </div>
           </div>
-      </article>
-   </section>
-   </>
-  )
-}
-  
+        </article>
+
+        <section className="flex w-full justify-center md:justify-start mt-4 px-4">
+            <p className="text-white text-center md:text-left ml-0 md:ml-5 mt-20">
+              ¿Problemas al iniciar sesión? <a href="forgot-password" className="text-[#DBFF5E]">Reestablece tu contraseña</a>
+            </p>
+          </section>
+      </section>
+    </>
+  );
+};
+
