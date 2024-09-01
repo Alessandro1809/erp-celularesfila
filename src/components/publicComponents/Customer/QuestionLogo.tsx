a04641ffd24a0f71e89723678fdad4c1ff1883d1
cesarm23 <crmorales2316@gmail.com>
2024-08-31 20:27:12 -0600

    Se agregan todas las vistas para el customer junto con el auth & layout

---
+++ src/components/publicComponents/Customer/QuestionLogo.tsx
@@ -0,0 +1,6 @@
+import  { SVGProps } from 'react';
+export function QuestionLogo(props: SVGProps<SVGSVGElement>) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="40px" height="40px" viewBox="0 0 24 24" {...props}><path fill="none" stroke="white" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 8a3.5 3 0 0 1 3.5-3h1A3.5 3 0 0 1 16 8a3 3 0 0 1-2 3a3 4 0 0 0-2 4m0 4v.01"></path></svg>
+    )
+  }
